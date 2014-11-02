3db83773e0d473aaa46be1da0b1b543de237e637
Torsten Spickhofen <torsten.spickhofen@fu-berlin.de>
2014-11-02 21:01:31 +0100

    Added missing signals and fixed random Sweet

--- betofahl/betofahl.go
+++ betofahl/betofahl.go
@@ -4,28 +4,44 @@ package betofahl
 import (
 	"os"
 	"os/signal"
+	"time"
 	"syscall"
 	"fmt"
-	"time"
+	"math/rand"
 )
 
 var (
 	Texts = []string{"Schokodrops", "Gummibärchen", "Maoam", "Schokolade", "Lolli", "Colafläschchen"}
+	c = make(chan os.Signal)
 )
 
 func init() {
 	go func() {
 		// Set up termination detection
-		c := make(chan os.Signal)
-		signal.Notify(c, syscall.SIGKILL, syscall.SIGQUIT)
+		signal.Notify(c, syscall.SIGKILL, syscall.SIGQUIT, os.Interrupt, syscall.SIGTERM)
 
 		// Wait for termination
 		<-c
 
+		// Randomize choice
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 		// Generate random text
-		text := Texts[int(time.Now().Unix()) % len(Texts)]
+		text := Texts[r.Intn(len(Texts))]
 
 		// Voll fair
 		fmt.Println(text)
+		os.Exit(0)
 	}()
+}
+
+func Defer() {
+	// Randomize choice
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// Generate random text
+	text := Texts[r.Intn(len(Texts))]
+
+	// Voll fair
+	fmt.Println(text)
 }
--- testbetofahl/testbetofahl.go
+++ testbetofahl/testbetofahl.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	_ "betofahl"
+	"github.com/hawkril/betofahl/betofahl"
 	"fmt"
 	"time" 
 )
 
 func main(){
+	defer betofahl.Defer()
+
 	fmt.Println("Ïch laufe")
 	<-time.NewTimer(30*time.Second).C
 }
